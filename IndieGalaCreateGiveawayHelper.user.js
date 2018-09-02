8ed2ca65f86adcdbba8d06c2a9274d64c8150c94
mrmarble <4268580+MrMarble@users.noreply.github.com>
2018-09-02 19:40:29 +0200

    Initial commit, still in progress

---
+++ IndieGalaCreateGiveawayHelper.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         IndieGala Create Giveaway Helper
+// @namespace    https://github.com/MrMarble/IndieGalaCreateGiveawayHelper
+// @version      0.1
+// @description  Creating a giveaway is now  a lot easier!!
+// @author       MrMarble
+// @updateURL    https://raw.githubusercontent.com/MrMarble/IndieGalaCreateGiveawayHelper/master/waitForKeyElements.js
+// @downloadURL  https://raw.githubusercontent.com/MrMarble/IndieGalaCreateGiveawayHelper/master/waitForKeyElements.js
+// @match        https://www.indiegala.com/profile?user_id=*
+// @match        https://www.indiegala.com/gift?gift_id=*
+// @require      https://raw.githubusercontent.com/MrMarble/IndieGalaCreateGiveawayHelper/master/waitForKeyElements.js
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @resource     style https://raw.githubusercontent.com/MrMarble/IndieGalaCreateGiveawayHelper/master/style.css
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  $.noConflict();
+  run();
+
+  function run() {
+    if (!'URLSearchParams' in window) {
+      throw "Web Browser not supported!";
+    }
+
+    let searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('user_id')) {
+      fillGiveaway(searchParams);
+    } else if (searchParams.has('gift_id')) {
+      GM_addStyle(GM_getResourceText('style'));
+      waitForKeyElements("#steam-key-games", addButton, true);
+    }
+  }
+
+  function addButton() {
+    jQuery('div[id^="serial_"]').each((i, element) => {
+      jQuery(element).append('<div class="entry-elem align-c create-giveaway-helper"><i aria-hidden="true" class="fa fa-gift"></i></div>');
+      let game_url = jQuery(element).parent('.game-key-string').find('a.game-steam-url').attr('href');
+      let serial = jQuery(element).find('input[id^=serial_n_]').val();
+    });
+
+  }
+
+  function fillGiveaway(searchParams) {
+    //TODO Create this function
+  }
+})();
